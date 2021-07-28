6163d15c8524a97492193352f4b4671be8c5c213
Jeremy <j.gautrais@gmail.com>
2021-07-28 11:45:33 +0200

    Update url

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = "https://8dyqz-3001.sse.codesandbox.io/persons/";
+const baseURL = "https://fullstackopencourse.vercel.app/persons/";
 
 const getAll = () => {
   const request = axios.get(baseURL);
